ccffdfd013bf9a6289fe48842d5b483223fec052
Ericson Joseph <ericsonjoseph@gmail.com>
2021-06-27 19:10:30 -0300

    first commit

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+setuptools.setup(
+    name="wgit",
+    version="0.0.1",
+    author="Ericson Joseph",
+    author_email="ericsonjoseph@gmail.com",
+    description="Wrapper of git for multiple accounts and servers",
+    scripts = ['wgit'],
+    url="https://github.com/ericsonj/wgit",
+    license="MIT",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'giturlparse',
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
